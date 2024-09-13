459b5d6c50f452c31c529d1b0862ab1d2767377a
Mushthak VP <mail.musthak@gmail.com>
2024-09-13 10:00:22 +0530

    user and user colour added

---
+++ schema/userSchema.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+
+    },
+    profileImage: {
+        type: String,
+    },
+    username: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    employeeId: {
+        type: String,
+        unique: true,
+    },
+    phone: {
+        type: String,
+    },
+    designation: {
+        type: String,
+    },
+    dateOfBirth: {
+        type: Date,
+    },
+    salary: {
+        type: Number,
+    },
+    password: {
+        type: String,
+    },
+    otp: {
+        type: String,
+    },
+
+    otpTimestamp: { type: Date },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'manager', 'teamlead', 'user', 'client', 'watcher', 'tester'],
+        default: 'user'
+    },
+    projects: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Board',
+    }],
+
+    fcmTokens: {
+        type: [String],
+        default: []
+    },
+    checkInStartTime: {
+        type: Date
+    },
+    checkinEndTime: {
+        type: Date
+    },
+    checkOutStartTime: {
+        type: Date
+    },
+    checkoutEndTime: {
+        type: Date
+    }
+
+}, { timestamps: true });
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
